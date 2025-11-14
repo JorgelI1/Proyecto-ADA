e8e33f9f7e6286600ea21627806f8bc126cfbacb
Wind <john.050521@gmail.com>
2025-11-14 13:47:25 -0500

    nodo

---
+++ src/Nodo.java
@@ -0,0 +1,22 @@
+public class Nodo {
+    int dato;
+    Nodo siguiente;
+    Nodo anterior;
+
+    public Nodo() {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
+public class Nodo {
+    int dato;
+    Nodo siguiente;
+    Nodo anterior;
+
+    public Nodo() {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
